8232889edd6c638d1e0d98218022d767f955a507
alexneo68 <tunning_brain@mail.ru>
2021-11-08 19:38:22 +0300

    first commit

---
+++ database/migrations/2021_11_08_162849_create_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body');
+
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
